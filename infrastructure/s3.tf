fc1ab0477fd6d6db1e6ef7b4cf609c29bae7d58e
RafaelSanchezCordoba <rafa.tivuron@gmail.com>
2026-09-24 11:43:42 +0200

    Set up S3 and CloudFront infrastructure with Terraform

---
+++ infrastructure/s3.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "${var.project_name}-aws-rsdev"
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "website" {
+  statement {
+    actions = ["s3:GetObject"]
+
+    resources = [
+      "${aws_s3_bucket.website.arn}/*"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+
+      values = [
+        aws_cloudfront_distribution.website.arn
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.website.json
+}
