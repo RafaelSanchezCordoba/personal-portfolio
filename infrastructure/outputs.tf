7c38ede540c7505d85dfd9bc3fc055b2ae1c99a0
RafaelSanchezCordoba <rafa.tivuron@gmail.com>
2026-09-24 14:19:56 +0200

    Add Terraform outputs for custom domain

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -9,3 +9,11 @@ output "cloudfront_distribution_id" {
 output "cloudfront_distribution_domain_name" {
     value = aws_cloudfront_distribution.website.domain_name
 }
+
+output "custom_domain" {
+  value = "https://aws.rafasanchez.dev"
+}
+
+output "certificate_arn" {
+  value = aws_acm_certificate.website.arn
+}
